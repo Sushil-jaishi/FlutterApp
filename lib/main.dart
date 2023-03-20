7a76053bc90a5243e22aace0740f51be7bd2472b
Sushil Jaishi <sushiljaishi@gmail.com>
2023-03-20 13:41:13 +0545

    CircleAvatar

--- lib/main.dart
+++ lib/main.dart
@@ -20,21 +20,9 @@ class HomePageScreen extends StatelessWidget{
         appBar: AppBar(
           title: Text('Sushil'),
         ),
-        body:Column(
-          //usage pf Row widget is also similar as column widget
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.green,
-            ),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-            )
-          ],
+        body:CircleAvatar(
+          backgroundImage: AssetImage('assets/images/image.jpg'),
+          radius: 100,
         )
     );
   }
