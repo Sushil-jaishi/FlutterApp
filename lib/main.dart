96ac7b4045653f5b4429427c5cb253ae25e9efdb
Sushil Jaishi <sushiljaishi@gmail.com>
2023-03-20 10:08:05 +0545

    New Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'hello',
+      theme: ThemeData( primarySwatch: Colors.green),
+      home: HomePageScreen(),
+    );
+  }
+}
+class HomePageScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Sushil'),
+        ),
+        body:
+    );
+  }
+
+}
